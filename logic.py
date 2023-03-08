e0fad7bacaac78a0a07ace46d9e9e891ba47dd9d
fbizza <francesco.bizzarri@stud.unifi.it>
2023-03-08 14:20:32 +0100

    Add basic game logic function

--- logic.py
+++ logic.py
@@ -1,3 +1,5 @@
+import random
+
 board = ["Via!", "Vicolo Corto", "Probabilità_1", "Vicolo Stretto", "Tassa Patrimoniale", "Stazione SUD",
          "Bastioni Gran Sasso", "Imprevisti_1", "Viale Monterosa", "Viale Vesuvio", "Prigione/Transito", "Via Accademia",
          "Società Elettrica", "Corso Ateneo", "Piazza Università", "Stazione OVEST", "Via Verdi",
@@ -6,3 +8,16 @@ board = ["Via!", "Vicolo Corto", "Probabilità_1", "Vicolo Stretto", "Tassa Patr
          "Società Acqua Potabile", "Piazza Giulio Cesare", "In Prigione!", "Via Roma", "Corso Impero",
          "Probabilità_3", "Largo Augusto", "Stazione EST", "Imprevisti_3", "Viale dei Giardini",
          "Tassa del Lusso", "Parco della Vittoria"]
+
+num_rolls = 100000
+go_to_jail = board.index("In Prigione!")
+jail = board.index("Prigione/Transito")
+
+def roll_dice():
+    return random.randint(1, 6)
+
+def update_position(current_position, dice_1, dice_2):
+    if (current_position + dice_1 + dice_2) % len(board) == go_to_jail:
+        return jail
+    else:
+        return (current_position + dice_1 + dice_2) % len(board)
